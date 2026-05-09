f4e85b85c922f905b8d5ba0cb9ade6a4acc7f2ff
goyalk01 <cocplaner9897@gmail.com>
2026-05-09 19:38:30 +0530

    Fix: remove --enable-preview, clean repo artifacts, stabilize build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("com.android.library") version "8.5.0" apply false
-    kotlin("android") version "2.0.0" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
-        kotlin("kapt") version "2.0.0" apply false
+    kotlin("android") version "2.0.21" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+    kotlin("kapt") version "2.0.21" apply false
 }
 
 // Shared version constants for all modules
